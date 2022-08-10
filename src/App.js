740d2cd88dfa9a21ed5899feb98e6365caeeb0d6
Panida Auitrakun <panidaauitrakun1234@gmail.com>
2022-08-10 17:22:00 +0700

    Part 4 Components & Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import './App.css';
 import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
 
+const word = 'Hello';
 function App() {
+
   return (
+
     <div>
-      <CharacterCard value = 'h'/>
-      <CharacterCard value = 'i'/>
+      {
+        <WordCard value = {word}/>
+      }
     </div>
+
   );
 }
 
