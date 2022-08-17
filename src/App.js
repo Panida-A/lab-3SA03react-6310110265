3a8d707af16c9bd8cc600a2a0f813f7cd655602f
Panida Auitrakun <panidaauitrakun1234@gmail.com>
2022-08-18 00:25:27 +0700

    Part 7.3

--- src/App.js
+++ src/App.js
@@ -20,31 +20,30 @@ function App() {
   return (
 
     <div className="button-htp">
-      <main>
-      <br></br>
-        <h1>
-          Game Logic
-        </h1>        
-        <WordCard value = {word}/>
-        <div className="htp">
-          <button className="how2p" onClick={() => setButtonPopup(true)}>How to play?</button>
-        </div>
-        
-      </main>    
+
+      <div className="wrapper">
+
+        <main>
+        <br></br>
+          <h1>Game Logic</h1>        
+          <WordCard value = {word}/>
+
+          <div className="htp">
+            <button className="how2p" onClick={() => setButtonPopup(true)}>How to play?</button>
+          </div>
+          <br></br>
+          
+        </main>  
+
+      </div>
 
       <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
-          <h3>How to play?</h3>
-          <br></br>
-          <p>Choose letters to connect to the correct words.</p>
-      </Popup>   
-
-      {/*<Popup trigger={timedPopup} setTrigger={setTimedPopup}>
-          
-        </Popup>*/}
+        <h3>How to play?</h3>
+        <br></br>
+        <p>Choose letters to connect to the correct words.</p>
+      </Popup>        
 
     </div>
-
-
   );
 }
 
